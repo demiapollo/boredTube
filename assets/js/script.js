f372999569fe9a5cd0bc93893db0c1b44544ecd7
demiapollo <apollodemirel67@gmail.com>
2023-10-05 20:40:16 -0700

    Bored API & YouTube API call functions are fixed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,93 +6,68 @@ let youtubeApiKeys = [
 const API_KEY =
   youtubeApiKeys[Math.floor(Math.random() * youtubeApiKeys.length)];
 
-const welcome = document.querySelector(".welcome");
-const carousel = document.querySelector(".carouselbox");
-const next = document.querySelector(".next");
-const back = document.querySelector(".back");
 
-// Calling bored api to get a random activity
-const boredApi = function (customURL) {
-  fetch(`https://www.boredapi.com/api/activity${customURL}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (boredapi) {
-      if (boredapi.activity === undefined) {
-        resultsHeader.textContent =
-          "Sorry, no results found. Please try again.";
-        return;
-      }
+  document.getElementById('generate').addEventListener('click', generateActivity);
 
-      boredActivity = boredapi.activity;
-      resultsHeader.textContent = "Activity: " + boredActivity;
+async function generateActivity() {
+    try {
+        // Fetch random activity from Bored API
+        const response = await fetch('https://www.boredapi.com/api/activity');
+        const data = await response.json();
+        document.getElementById('activity').innerText = data.activity;
 
-      youTubeApi(boredapi);
-    });
-};
+        // Fetch related videos from YouTube API
+        const query = encodeURIComponent(data.activity);
+        const apiKey = API_KEY;
+        // Add maxResults=15 to the query string to get 5 videos
+        const youtubeResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=15&key=${apiKey}`);
+        const youtubeData = await youtubeResponse.json();
 
-// Calling youtube api to get a video based on the activity from bored api
-const youTubeApi = function (boredapi) {
-  fetch(
-    `${url}?key=${API_KEY}&type=video&part=snippet&maxResults=5&q=${boredapi.activity}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (newdata) {
-      data = newdata;
-      displayVideos(data);
-    });
-};
-
-// Displaying the video on the page
-const displayVideos = function (data) {
-  navigate(0);
-};
-
-// Function to navigate through the videos
-function navigate(direction) {
-  index += direction;
-  if (index < 0) {
-    index = data.items.length - 1;
-  } else if (index > data.items.length - 1) {
-    index = 0;
-  }
-
-  currentImage = data.items[index];
-  carouselVideo = document.querySelector(".videoNow");
-  carouselVideo.setAttribute (
-    "src",
-    `https://www.youtube.com/embed/${currentImage.id.videoId}`
-  );
-  videoName = currentImage.snippet.title;
-  videoImage = currentImage.id.videoId;
+        // Display videos in the carousel
+        displayVideos(youtubeData.items);
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Failed to fetch data. Please try again.');
+    }
 }
 
-// Event listeners for the back and next buttons
-carousel.addEventLister("click", function () {
-  window.location.href = data[index];
-}); 
+function displayVideos(videos) {
+    const carousel = document.getElementById('carousel');
+    carousel.innerHTML = ''; // Clear previous videos
 
-next.addEventListener("click", function (event) {
-  event.stopPropogation();
-  navigate(1);
-});
+    videos.forEach(video => {
+        const videoElement = document.createElement('iframe');
+        videoElement.src = `https://www.youtube.com/embed/${video.id.videoId}`;
+        videoElement.width = 560;
+        videoElement.height = 315;
+        carousel.appendChild(videoElement);
+        // center videos
+        videoElement.style.transform = `translateX(${-100 * videos.length}%)`;
+        // show carousel buttons with videos
+        document.getElementById('prev').style.display = 'inline-block';
+        document.getElementById('next').style.display = 'inline-block';
+        
 
-back.addEventListener("click", function (event) {
-  event.stopPropogation();
-  navigate(-1);
-});
 
+    });
 
-// activity type part
-function activityTypeChoice (objButton1) {
-  const activityType = objButton1.value;
-  $(".activityType").fadeOut(1000);
-  setTimeout(() => {
-    $("#soloOrGroup").fadeIn(1000).css("display", "flex");
-  }, 1000); 
+    // Implement carousel navigation
+    let currentIndex = 0;
+    updateCarousel();
+
+    document.getElementById('next').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % videos.length;
+        updateCarousel();
+    });
+
+    document.getElementById('prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + videos.length) % videos.length;
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        Array.from(carousel.children).forEach((iframe, index) => {
+            iframe.style.transform = `translateX(${-100 * currentIndex}%)`;
+        });
+    }
 }
-
-// cost part
-
